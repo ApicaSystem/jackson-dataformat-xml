c1f159edf6ca70715a0000613c63208daf901f98
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-05-24 13:42:36 -0700

    Fix #405
    # Conflicts:
    #	release-notes/VERSION-2.x
    #	src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeBasicDeserTest.java
    #	src/test/java/com/fasterxml/jackson/dataformat/xml/deser/UntypedObjectDeserTest.java

--- src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeBasicDeserTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeBasicDeserTest.java
@@ -2,10 +2,11 @@ package com.fasterxml.jackson.dataformat.xml.deser;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
-import org.junit.Ignore;
 
 public class JsonNodeBasicDeserTest extends XmlTestBase
 {
@@ -19,8 +20,8 @@ public class JsonNodeBasicDeserTest extends XmlTestBase
         assertEquals("123", root.get("attr").textValue());
     }
 
-    // [dataformat-xml#403]: Allow sequences
     /* //dups handling from databind 2.12 is required
+    // [dataformat-xml#403]: Allow sequences
     public void testRepeated() throws Exception
     {
         JsonNode root = XML_MAPPER.readTree("<root><value>a</value><value>b</value></root>");
@@ -31,6 +32,24 @@ public class JsonNodeBasicDeserTest extends XmlTestBase
         assertEquals(2, arr.size());
         assertEquals("a", root.at("/value/0").asText());
         assertEquals("b", root.at("/value/1").asText());
+    }
+
+    // [dataformat-xml#405]: support mixed content
+    public void testMixedContent() throws Exception
+    {
+        JsonNode fromXml = XML_MAPPER.readTree("<root>first<a>123</a>second<b>abc</b>last</root>");
+        final ObjectNode exp = XML_MAPPER.createObjectNode();
+        exp.putArray("")
+            .add("first")
+            .add("second")
+            .add("last");
+        exp.put("a", "123");
+        exp.put("b", "abc");
+        
+        if (!fromXml.equals(exp)) {
+            ObjectWriter w = new JsonMapper().writerWithDefaultPrettyPrinter();
+            fail("Expected:\n"+w.writeValueAsString(exp)+"\ngot:\n"+w.writeValueAsString(fromXml));
+        }
     }
     */
 }
