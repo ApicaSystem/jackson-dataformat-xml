492ba5c0c7d706bfe9d14df5bca8df3bc0150cd6
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-06-05 15:53:54 -0700

    Fixed #226
    # Conflicts:
    #	release-notes/VERSION-2.x
    #	src/test/java/com/fasterxml/jackson/dataformat/xml/failing/MixedContentTreeRead226Test.java

--- src/test/java/com/fasterxml/jackson/dataformat/xml/failing/JsonNodeMixedContent402Test.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/failing/JsonNodeMixedContent402Test.java
@@ -39,4 +39,16 @@ public class JsonNodeMixedContent402Test extends XmlTestBase
         assertEquals(JSON_MAPPER.readTree(a2q("{'':['first','second','third'],'a':1,'b':'2','':'after'}")),
                 XML_MAPPER.readTree("<root>first<a>1</a>second<b>2</b>this</root>"));
     }
+
+    // [dataformat-xml#226]
+    public void testMixed226() throws Exception
+    {
+        final String XML = "<root>\n"
+                +"<a>mixed1 <b>leaf</b>"
+                +" mixed2</a>\n"
+                +"</root>";
+        assertEquals(JSON_MAPPER.readTree(
+                a2q("{'a':{'':['mixed1 ',' mixed2'],'b':'leaf'}}")),
+                XML_MAPPER.readTree(XML));
+    }
 }
